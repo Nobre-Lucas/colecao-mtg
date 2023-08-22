ab880656e0ffeee37e3e12c75c393e9c06a1b96a
Lucas Nobre Barbosa <lucasbnobre18@gmail.com>
2023-08-21 22:11:29 -0300

    feat: add new endpoint for card search by name

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -1,3 +1,5 @@
+import * as Scry from 'scryfall-sdk';
+
 import { Router } from 'express';
 
 const router = Router();
@@ -5,6 +7,18 @@ const router = Router();
 router.get('/', (_, res) => {
     return res.send('Bem vindo à sua coleção de Magic the Gathering!');
 });
+
+router.post('/search-cards', async (req, res) => {
+
+    const card = await Scry.Cards.autoCompleteName(req.body.nome);
+    return res.send(card);
+})
+
+router.post('/search-card', async (req, res) => {
+
+    const card = await Scry.Cards.autoCompleteName(req.body.nome);
+    return res.send(card[0]);
+})
 
 
 export { router };
